783941926a7d3ef4b203aedf1e86158bc8a3f507
Chris Spencer <cvspencer@gmail.com>
2018-01-29 15:36:12 +0900

    Fixed Slick library imports and set LWJGL natives path

---
+++ src/com/potatospy/Game.java
@@ -0,0 +1,17 @@
+package com.potatospy;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+
+public class Game extends StateBasedGame{
+
+
+    public static final String gamename="Ham Blaster!";
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
